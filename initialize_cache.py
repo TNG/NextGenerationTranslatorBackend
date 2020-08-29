e11b88b601bf716360940789c6c092030fa7c94c
Thomas Endres <thomas.endres@tngtech.com>
2020-08-29 20:30:42 +0200

    Removed torch cache setting

--- initialize_cache.py
+++ initialize_cache.py
@@ -1,7 +1,6 @@
 import torch
-import os
 
-torch.hub.set_dir(f"{os.path.dirname(os.path.realpath(__file__))}/cache")
 de2en = torch.hub.load('pytorch/fairseq', 'transformer.wmt19.de-en',
                        checkpoint_file='model1.pt:model2.pt:model3.pt:model4.pt',
                        tokenizer='moses', bpe='fastbpe')
+print(de2en.translate("Das hat geklappt"))
