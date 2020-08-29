9cdc837484940c51a4755e8b174d2a1436be6363
Thomas Endres <thomas.endres@tngtech.com>
2020-08-30 00:58:50 +0200

    Server is started while the model is still loading

--- translator.py
+++ translator.py
@@ -1,33 +1,38 @@
 from flask import Flask, request
 from flask_jsonpify import jsonify
 import torch
-import os
+from _thread import start_new_thread
 
 app = Flask(__name__)
+de2en = None
 
-torch.hub.set_dir(f"{os.path.dirname(os.path.realpath(__file__))}/cache")
-
-
-# de2en = torch.hub.load('pytorch/fairseq', 'transformer.wmt19.de-en',
-#                       checkpoint_file='model1.pt:model2.pt:model3.pt:model4.pt',
-#                       tokenizer='moses', bpe='fastbpe')
 
 @app.route('/health', methods=['GET', 'POST'])
 def health():
-    print("Health called")
     return jsonify(healthy=True)
 
 
 @app.route('/', methods=['POST'])
 def index():
-    print("Translate called")
     return jsonify(translation=translate(request.json['text']))
 
 
 def translate(text):
-    return text + " 1"
-#    return de2en.translate(text)
+    if de2en is not None:
+        return de2en.translate(text)
+    else:
+        return text + " 1"
+
+
+def load_model():
+    global de2en
+    de2en = torch.hub.load('pytorch/fairseq', 'transformer.wmt19.de-en',
+                           checkpoint_file='model1.pt:model2.pt:model3.pt:model4.pt',
+                           tokenizer='moses', bpe='fastbpe')
+    print("Model loaded")
+    print(de2en.translate("Alles funktioniert einwandfrei"))
 
 
 if __name__ == "__main__":
-    app.run(port=80, host='0.0.0.0', debug=False)
+    start_new_thread(load_model, ())
+    app.run(port=80, host='0.0.0.0', debug=False, threaded=True)
