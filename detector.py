c6b9e7ac71eed032692fc13c457dabbee573e305
Thomas Endres <thomas.endres@tngtech.com>
2020-09-27 11:46:20 +0200

    If an empty text is transmitted, "unknown" is returned

--- detector.py
+++ detector.py
@@ -1,8 +1,12 @@
 from langdetect import detect
+from langdetect.lang_detect_exception import LangDetectException
 
 
 class Detector:
 
     @staticmethod
     def detect_language(text):
-        return detect(text)
+        try:
+            return detect(text)
+        except LangDetectException:
+            return 'unknown'
